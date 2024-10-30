9e21b88ec6cb30372ad84e5ec0e1c9f25757e26a
Jeremy <jeremy.garcia@vtsd.com>
2024-10-30 12:27:27 -0400

    1-2-1 step 15-26

--- 1-2-1/step1.py
+++ 1-2-1/step1.py
@@ -1,12 +1,14 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
+import random as rand
+from pickle import GLOBAL
 
 #-----game configuration----
 spot_color = "pink"
 spot_size = 2
 spot_shape = "circle"
-
+score = 0
 #-----initialize turtle-----
 spot = trtl.Turtle()
 spot.shape(spot_shape)
@@ -14,9 +16,18 @@ spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
 
 #-----game functions--------
+def change_position():
+    new_xpos = rand.randint(1, 400)
+    new_ypos = rand.randint(1, 300)
+    spot.goto(new_xpos, new_ypos)
+def update_score():
+    global score
+    score = score + 1
+    print(score)
 def spot_clicked(x,y):
-    print("Hello World!")
-
+    spot.penup()
+    change_position()
+    update_score()
 #-----events----------------
 spot.onclick(spot_clicked)
 
