233fafbe4d9b397c4de4bfa14fa0d4b2ebf41a81
Jeremy <jeremy.garcia@vtsd.com>
2024-10-31 11:42:24 -0400

    1-2-1 step 26-40

--- 1-2-1/step1.py
+++ 1-2-1/step1.py
@@ -2,7 +2,6 @@
 #-----import statements-----
 import turtle as trtl
 import random as rand
-from pickle import GLOBAL
 
 #-----game configuration----
 spot_color = "pink"
@@ -11,19 +10,25 @@ spot_shape = "circle"
 score = 0
 #-----initialize turtle-----
 spot = trtl.Turtle()
+score_writer = trtl.Turtle()
 spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
-
+font_setup = ("Arial", 20, "normal")
 #-----game functions--------
+score_writer.penup()
+score_writer.goto(-200, 200)
+score_writer.hideturtle()
 def change_position():
     new_xpos = rand.randint(1, 400)
     new_ypos = rand.randint(1, 300)
     spot.goto(new_xpos, new_ypos)
 def update_score():
     global score
-    score = score + 1
-    print(score)
+    score += 1
+    score_writer.write(score, font=font_setup)
+    score_writer.clear()
+    score_writer.write(score, font=font_setup)
 def spot_clicked(x,y):
     spot.penup()
     change_position()
