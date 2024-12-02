8ec66ff0cfac7a4b5d0b268b4aa7da6d835a38e5
Jeremy <jeremy.garcia@vtsd.com>
2024-12-02 13:09:55 -0500

    1-2-4 maze barriers

---
+++ 1-2-4/spiral_maze.py
@@ -0,0 +1,39 @@
+import turtle as trtl
+
+# Initialize variable
+wn = trtl.Screen()
+maze_painter = trtl.Turtle()
+length = 35
+path_width = 30
+
+# setup Turtle
+maze_painter.left(90)
+maze_painter.pensize(5)
+maze_painter.speed(0)
+
+#draw maze
+#process
+#draw line
+#turn left
+#increment length
+#repeat
+
+def draw_barrier():
+    maze_painter.right(90)
+    maze_painter.forward(path_width)
+    maze_painter.backward(path_width)
+    maze_painter.left(90)
+
+for wall in range(21):
+    maze_painter.forward(length/3)
+    maze_painter.penup()
+    maze_painter.forward(path_width)
+    maze_painter.pendown()
+    if (wall > 5):
+        draw_barrier()
+    maze_painter.forward(length- path_width - (length/3))
+    maze_painter.left(90)
+    length += 15
+
+wn.listen()
+wn.mainloop()
