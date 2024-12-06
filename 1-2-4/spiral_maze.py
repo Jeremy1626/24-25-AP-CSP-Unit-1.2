a5d3e77bb14e4972c08cdc1695cb58a5cb62f796
Jeremy <jeremy.garcia@vtsd.com>
2024-12-06 13:09:28 -0500

    1-2-4 maze complete

--- 1-2-4/spiral_maze.py
+++ 1-2-4/spiral_maze.py
@@ -1,39 +1,46 @@
 import turtle as trtl
+import random as rand
 
-# Initialize variable
+# Intialize Variables
 wn = trtl.Screen()
 maze_painter = trtl.Turtle()
-length = 35
 path_width = 30
 
-# setup Turtle
+
+# Setup Turtle
 maze_painter.left(90)
 maze_painter.pensize(5)
 maze_painter.speed(0)
 
-#draw maze
-#process
-#draw line
-#turn left
-#increment length
-#repeat
-
+# Draw Maze
+# Process:
+# Draw a line
+# Turn Left
+# Increment Length
+# Repeat
 def draw_barrier():
     maze_painter.right(90)
     maze_painter.forward(path_width)
     maze_painter.backward(path_width)
     maze_painter.left(90)
 
+length = 35
 for wall in range(21):
-    maze_painter.forward(length/3)
+    gap_space = rand.randint(0, length - path_width)
+    door_space = 0
+    maze_painter.forward(gap_space)
     maze_painter.penup()
     maze_painter.forward(path_width)
     maze_painter.pendown()
-    if (wall > 5):
-        draw_barrier()
-    maze_painter.forward(length- path_width - (length/3))
+    if(wall > 5):
+       door_space = rand.randint(0, length - path_width - gap_space)
+       maze_painter.forward(door_space)
+       draw_barrier()
+    maze_painter.forward(length - gap_space - path_width - door_space)
     maze_painter.left(90)
     length += 15
+
+
 
 wn.listen()
 wn.mainloop()
